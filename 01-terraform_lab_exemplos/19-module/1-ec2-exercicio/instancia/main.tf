c731549e8a5aea3257a4017c27afb747ba5e7d1c
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-14 14:37:08 -0300

    Update main.tf

--- 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/main.tf
+++ 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/main.tf
@@ -3,9 +3,21 @@ terraform {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.tamanho
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = ["${aws_security_group.permitir_ssh.id}"]
+  count         = var.quantidade
+  key_name      = "key_matheus_dev_ubunto"
+  
+  associate_public_ip_address = true
+  
+  root_block_device {
+    volume_size = 8
+    encrypted = true
+  }
+    
   tags = {
-    Name = "${var.nome}"
+    Name = "ec2-matheus-${var.nome}-${count.index}"
   }
 }
