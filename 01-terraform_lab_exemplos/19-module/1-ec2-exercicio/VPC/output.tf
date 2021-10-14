1c470f1568a9e3716f666cb0c89e2424f27a19a9
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-14 15:31:27 -0300

    Update output.tf

--- 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/VPC/output.tf
+++ 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/VPC/output.tf
@@ -2,6 +2,6 @@ output "vpc_id" {
   value = aws_vpc.my_vpc.id
 }
 
-#output "subnet_id" {
-  #value = "${data.aws_subnet.test_subnet.*.id}"
-#}
+output "subnet_id" {
+  value = aws_vpc.my_subnet.id
+}
