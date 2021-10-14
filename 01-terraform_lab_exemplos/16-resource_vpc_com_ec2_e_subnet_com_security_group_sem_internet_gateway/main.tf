30df936b582c4582d802c6bcb8152c3f8b56342c
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-14 10:17:56 -0300

    Update main.tf

--- 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet_com_security_group_sem_internet_gateway/main.tf
+++ 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet_com_security_group_sem_internet_gateway/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
 }
 
 resource "aws_instance" "web" {
   ami                     = data.aws_ami.ubuntu.id
-  instance_type           = "t3.micro"
-  key_name                = "Itau_treinamento" # key chave publica cadastrada na AWS 
+  instance_type           = "t2.micro"
+  key_name                = "key_matheus_dev_ubunto" # key chave publica cadastrada na AWS 
   subnet_id               =  aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
   private_ip              = "172.16.10.101"
   # vpc_security_group_ids  = ["${aws_security_group.allow_ssh1.id}"]
@@ -17,7 +17,7 @@ resource "aws_instance" "web" {
 
 resource "aws_instance" "web2" {
   ami                     = data.aws_ami.ubuntu.id
-  instance_type           = "t3.micro"
+  instance_type           = "t2.micro"
   key_name                = "Itau_treinamento" # key chave publica cadastrada na AWS 
   subnet_id               =  aws_subnet.my_subnet_b.id # vincula a subnet direto e gera o IP automático
   private_ip              = "172.16.20.100"
