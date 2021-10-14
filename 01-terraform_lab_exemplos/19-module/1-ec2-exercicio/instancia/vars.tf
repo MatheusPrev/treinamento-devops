bcddb960e161f2f0de6a6f0f75757b52e36ac448
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-14 14:47:52 -0300

    Update vars.tf

--- 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/vars.tf
+++ 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/vars.tf
@@ -1,5 +1,24 @@
-
 variable "nome" {
   type = string
   description = "Digite o nome da instancia"
+}
+
+variable "subnet_id" {
+  description = "Digite a Subnet onde deseja criar a máquina: "
+  validation {
+    condition     = length(var.subnet_id) > 7 && substr(var.subnet_id, 0, 7) == "subnet-"
+    error_message = "O valor da subnet_id não é válido, tem que começar com \"subnet-\"."
+  }
+}
+
+variable "tamanho" {
+  description = "Digite qual é o tamanho da máquina (large, micro...): "
+  validation {
+    condition     = length(var.tamanho) > 3 && substr(var.tamanho, 0, 3) == "t2."
+    error_message = "O valor do tamanho não é válido, tem que começar com \"t2.\"."
+  }
+}
+
+variable "quantidade" {
+  description = "Digite a quantidade de máquinas que deseja: "
 }
