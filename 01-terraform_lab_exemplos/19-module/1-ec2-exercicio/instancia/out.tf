5f020b29af2cc70869b81befe74c3acb20ef9121
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-14 14:56:46 -0300

    Update out.tf

--- 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/out.tf
+++ 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/out.tf
@@ -2,7 +2,3 @@ output name {
   value       = aws_instance.web.public_dns
   description = "DNS da instância"
 }
-
-output "ssh_connect" {
-  value =  "ec2 ${key+1} - ${item.private_ip} - ssh -i ~/.ssh/id_rsa ubuntu@${item.private_ip}"
-}
