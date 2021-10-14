cc3ad98de0f0547870599f0e6da4d579c74f8186
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-14 15:10:18 -0300

    Update main.tf

--- 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/main.tf
+++ 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/main.tf
@@ -2,7 +2,12 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-module "criar_instancia" {
-  source = "./instancia"
-  nome = "deploy"
+module "criar_vpc" {
+  source = "./VPC"
+  #nome = "deploy"
 }
+
+#module "criar_instancia" {
+  #source = "./instancia"
+  #nome = "deploy"
+#}
