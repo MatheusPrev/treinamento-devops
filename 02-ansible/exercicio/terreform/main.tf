4dce0f53af0ebd50564e6d05bba87f36e04fe939
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-15 15:45:55 -0300

    Update main.tf

--- 02-ansible/exercicio/terreform/main.tf
+++ 02-ansible/exercicio/terreform/main.tf
@@ -1,1 +1,13 @@
-ola
+provider "aws" {
+  region = "sa-east-1"
+}
+
+module "criar_vpc" {
+  source = "./VPC"
+}
+
+module "criar_instancia" {
+  source = "./instancia"
+  subnet_id = module.criar_vpc.subnet_id
+  vpc_id = module.criar_vpc.vpc_id
+}
