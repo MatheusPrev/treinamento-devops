4544f7571bebc710a4ab42871ed82ee12b33d6a4
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-19 10:15:04 -0300

    Update main.tf

--- 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
+++ 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
@@ -11,7 +11,7 @@ data "aws_ami" "ubuntu" {
 	  owners      = ["099720109477"]
 	  filter {
 	    name   = "name"
-	    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-*"]
+	    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server*"]
 	  }
 	}
 
